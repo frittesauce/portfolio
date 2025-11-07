5b9a8ee6cb179be783bec1553035a29f840101a7
frittesauce <181068943+frittesauce@users.noreply.github.com>
2025-11-07 14:13:56 +0100

    alot of things changed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>this is my website!</p>
+<img src="https://media1.tenor.com/m/_15yYkqVX8EAAAAd/cat-vibing.gif" alt="dance cat" />
